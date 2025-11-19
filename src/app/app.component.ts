884a8af7cae57a939a07cb97511287afe48f9f05
Jbadjei5 <165714771+Jbadjei5@users.noreply.github.com>
2025-11-19 12:55:21 +0000

    Add placeholder title method to AppComponent

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,6 +9,9 @@ import { StudentCardComponent } from './student-card/student-card.component';
   templateUrl: './app.component.html',
 })
 export class AppComponent {
+  title(title: any) {
+    throw new Error('Method not implemented.');
+  }
   students = [
     {
       id: 1,
